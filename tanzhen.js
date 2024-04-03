9e2cdae710464014c5d33e12fa6e6e6e4dee6312
baipiaoking88 <108050351+baipiaoking88@users.noreply.github.com>
2024-04-03 23:59:02 +0800

    Update tanzhen.js

--- tanzhen.js
+++ tanzhen.js
@@ -33,6 +33,6 @@ var avatar=document.querySelector(".item img")
 var footer=document.querySelector("div.is-size-7")
 footer.innerHTML="探针"
 footer.style.visibility="visible"
-avatar.src="https://fastly.jsdelivr.net/gh/baipiaoking88/picx-images-hosting@master/akemi1.64u3k1qot3o0.webp"
+avatar.src="https://fastly.jsdelivr.net/gh/baipiaoking88/mypic/akemi1.webp"
 avatar.style.visibility="visible"
 }
