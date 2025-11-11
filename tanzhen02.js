fe439637d6905d9fae0643631f2c0138f5435460
baipiaoking88 <108050351+baipiaoking88@users.noreply.github.com>
2025-11-11 23:06:00 +0800

    Add files via upload

---
+++ tanzhen02.js
@@ -0,0 +1,41 @@
+<!--动画-->
+window.onload = function () {
+$('.nb-menu').css('visibility', 'visible');
+$('.footer').css('visibility', 'visible');
+$('.nb-menu .right.menu,.nb-menu .item')
+  .transition({
+     animation: 'fade down in',
+     interval: 200,
+     silent: true
+});
+$('.footer .container b,.footer .container small')
+  .transition({
+     animation: 'fade up in',
+     interval: 200,
+     silent: true
+});
+$('.accordion,.cards .card,table,.table tr,.service-status h2')
+  .transition({
+     animation: 'scale in',
+     interval: 150,
+     silent: true
+});
+const cpuRollElements = document.querySelectorAll('.cpuroll');
+cpuRollElements.forEach((content) => {
+   function updateContainerWidth() {
+     const containerWidth = content.offsetWidth;
+     content.style.setProperty('--container-width', `${containerWidth}px`);
+}
+ updateContainerWidth();
+     window.addEventListener('resize', updateContainerWidth);
+});
+    $('.cpucontent').addClass("rollanimation");
+    
+<!--Logo和版权-->
+var avatar=document.querySelector(".item img")
+var footer=document.querySelector("div.is-size-7")
+footer.innerHTML="探针"
+footer.style.visibility="visible"
+avatar.src="https://fastly.jsdelivr.net/gh/baipiaoking88/mypic/akemi1.webp"
+avatar.style.visibility="visible"
+}
